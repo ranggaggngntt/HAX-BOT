346b0890d7edea6c3e08a6927b8eeb8e8b622249
ranggaggngntt <ranggaakb48@yahoo.com>
2021-04-04 20:55:58 +0700

    update list region

--- lib/SMTP/getsmtp.py
+++ lib/SMTP/getsmtp.py
@@ -24,27 +24,27 @@ class GET_SMTP(object):
             return False
 
     def get_aws_region(self, response):
-    list_region = '''us-east-1
-    us-east-2
-    us-west-1
-    us-west-2
-    af-south-1
-    ap-east-1
-    ap-south-1
-    ap-northeast-1
-    ap-northeast-2
-    ap-northeast-3
-    ap-southeast-1
-    ap-southeast-2
-    ca-central-1
-    eu-central-1
-    eu-west-1
-    eu-west-2
-    eu-west-3
-    eu-south-1
-    eu-north-1
-    me-south-1
-    sa-east-1'''
+        list_region = '''us-east-1
+        us-east-2
+        us-west-1
+        us-west-2
+        af-south-1
+        ap-east-1
+        ap-south-1
+        ap-northeast-1
+        ap-northeast-2
+        ap-northeast-3
+        ap-southeast-1
+        ap-southeast-2
+        ca-central-1
+        eu-central-1
+        eu-west-1
+        eu-west-2
+        eu-west-3
+        eu-south-1
+        eu-north-1
+        me-south-1
+        sa-east-1'''
         reg = False
         for region in list_region.splitlines():
             if str(region) in response:
